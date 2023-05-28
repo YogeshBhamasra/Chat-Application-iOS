50e518a9e535a7863e7ee0dffb053f229fafb9a8
Yogesh <yogesh.rao@bigohtech.com>
2023-05-28 10:54:54 +0530

    Initial Commit

---
+++ Chat Application/Chat_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_ApplicationApp.swift
+//  Chat Application
+//
+//  Created by Yogesh Rao on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Chat_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat Application/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chat Application
+//
+//  Created by Yogesh Rao on 28/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
